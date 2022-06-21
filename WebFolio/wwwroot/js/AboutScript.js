71633c6d6143ece0b35fca0337cdb06c773eb368
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-21 18:58:59 +1200

    initializing the image posters to variables

--- WebFolio/wwwroot/js/AboutScript.js
+++ WebFolio/wwwroot/js/AboutScript.js
@@ -2,3 +2,5 @@
 const btnSoft = document.getElementById('btn-soft');
 const btnTech = document.getElementById('btn-tech');
 
+const imgLeft = document.querySelector('.img-poster');
+const imgRight = document.querySelector('.img-second-poster');
