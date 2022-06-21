3ba9f0426b141c0bd5b1f0fb8c4714ef96793d97
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-21 19:07:32 +1200

    successfully implemented btn hover with css and js

--- WebFolio/wwwroot/js/AboutScript.js
+++ WebFolio/wwwroot/js/AboutScript.js
@@ -4,3 +4,26 @@ const btnTech = document.getElementById('btn-tech');
 
 const imgLeft = document.querySelector('.img-poster');
 const imgRight = document.querySelector('.img-second-poster');
+
+function btnLeftShow() {
+    btnSoft.classList.remove('hidden');
+}
+
+function btnRightShow() {
+    btnTech.classList.remove('hidden');
+}
+
+function btnRightHide() {
+    btnTech.classList.add('hidden');
+}
+
+
+function btnLeftHide() {
+    btnSoft.classList.add('hidden');
+}
+
+imgLeft.addEventListener('mouseover', btnLeftShow);
+imgLeft.addEventListener('mouseout', btnLeftHide)
+
+imgRight.addEventListener('mouseover', btnRightShow);
+imgRight.addEventListener('mouseout', btnRightHide);
